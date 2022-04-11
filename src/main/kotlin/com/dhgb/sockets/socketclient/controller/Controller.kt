7bbfe42d452471eb8010a0377556d01cc97443f7
diegoguerreroborda <diego.guerrero02@uptc.edu.co>
2022-04-11 15:34:09 -0500

    fix: Send and received data server

--- src/main/kotlin/com/dhgb/sockets/socketclient/controller/Controller.kt
+++ src/main/kotlin/com/dhgb/sockets/socketclient/controller/Controller.kt
@@ -4,13 +4,13 @@ import com.dhgb.sockets.socketclient.sockets.MyStompSessionHandler
 import org.springframework.http.HttpStatus
 import org.springframework.messaging.simp.stomp.StompSession
 import org.springframework.messaging.simp.stomp.StompSessionHandler
-import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RestController
 import org.springframework.web.socket.client.WebSocketClient
 import org.springframework.web.socket.client.standard.StandardWebSocketClient
 import org.springframework.web.socket.messaging.WebSocketStompClient
+
 
 //Client
 @RestController
@@ -19,11 +19,11 @@ class Controller {
     private val transport: WebSocketClient = StandardWebSocketClient()
     private val stompClient: WebSocketStompClient = WebSocketStompClient(transport)
     private val sessionHandler: StompSessionHandler = MyStompSessionHandler()
-    val stompSession: StompSession = stompClient.connect(URL, sessionHandler).get()
+    private val stompSession: StompSession = stompClient.connect(URL, sessionHandler).get()
 
-    @GetMapping("/")
-    fun test(): String {
-        return "server on"
+    init {
+//        stompClient.messageConverter = MappingJackson2MessageConverter()
+        stompSession.subscribe(SUBSCRIBE, sessionHandler)
     }
 
     @PostMapping("/info")
@@ -31,7 +31,6 @@ class Controller {
         println("Se va a enviar $data")
         return try {
             stompSession.send(DESTINATION, passTextToArrayByteArray(data))
-//            stompSession.subscribe(SUBSCRIBE, sessionHandler)
             HttpStatus.OK
         }catch (e: Exception){
             HttpStatus.CONFLICT
@@ -43,8 +42,8 @@ class Controller {
     }
 
     companion object{
-        const val URL = "ws://localhost:8080/chat"
-        const val DESTINATION = "/app/chat"
-        const val SUBSCRIBE = "/topic/messages"
+        const val URL = "ws://localhost:8080/current"
+        const val DESTINATION = "/server/current"
+        const val SUBSCRIBE = "/client/messages"
     }
 }
